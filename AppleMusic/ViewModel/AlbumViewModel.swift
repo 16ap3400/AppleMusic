7f06f6ce3bec54b25bd1a03abf5a5c457c8ab69e
Alex Peterson <petersonale@umsystem.edu>
2021-05-07 14:25:59 -0600

    implemented non-explicit vs explicit selection, but api does not support

--- AppleMusic/ViewModel/AlbumViewModel.swift
+++ AppleMusic/ViewModel/AlbumViewModel.swift
@@ -10,9 +10,20 @@ import Foundation
 class AlbumViewModel: ObservableObject {
     
     @Published var albums: [Album] = [Album]()
+    @Published var explicit: Bool = true
+    
     
     func fetchTopAlbums() {
-        if let url  = URL(string: "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/25/explicit.json") {
+        
+        var contentType: String
+        
+        if explicit {
+            contentType = "explicit"
+        } else {
+            contentType = "non-explicit"
+        }
+        
+        if let url  = URL(string: "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/25/" + contentType + ".json") {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error == nil {
@@ -35,54 +46,3 @@ class AlbumViewModel: ObservableObject {
         }
     }
 }
-    
-//    private func Alamofetch() {
-//        AF.request("https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/25/explicit.json").responseJSON{ response in
-////            print("Value: \(response.value ?? "ERROR")")
-//
-//            if let json = response.value {
-//                if  (json as? [String : AnyObject]) != nil{
-//                    if let dictionaryArray = json as? Dictionary<String, Dictionary<String, AnyObject?>> {
-//                        let jsonArray = dictionaryArray["feed"]!["results"]!
-//                        let data = response.data
-//
-////                        print(response.value)
-//                        let json = try? JSONDecoder().decode(Feed.self, from: response.value as! Data)
-//
-//                        if let json = json {
-//                            for i in 0..<json.results.count {
-//                                print("\(i)th album: \n")
-//                                if let artistName = json.results[i].artistName as? String, let name = json.results[i].name as? String, let contentAdvisoryRating = json.results[i].contentAdvisoryRating as? String, let artworkUrl100 = json.results[i].artworkUrl100 as? String {
-//                                    self.albumList.append(Album(artistName: artistName, name: name, contentAdvisoryRating: contentAdvisoryRating, artworkUrl100: artworkUrl100))
-//                                }
-//                                if self.albums.count > 0 {
-//                                    print(self.albums[self.albums.count-1].artistName)
-//                                }
-//                            }
-//                        } else {
-//                            print("Error decoding JSON")
-//                            print(json)
-//                        }
-//
-//
-////                        if let jsonArray = jsonArray as? Array<Dictionary<String, AnyObject?>>{
-////                            for i in 0..<jsonArray.count{
-////                                print("\(i)th album: \n")
-////
-////                                let json = jsonArray[i]
-////                                if let artistName = json["artistName"] as? String, let name = json["name"] as? String, let contentAdvisoryRating = json["contentAdvvisoryRating"] as? String, let artURL = json["artworkUrl100"] as? String {
-////                                self.albums.append(Album(artistName: artistName, name: name, contentAdvisoryRating: contentAdvisoryRating, artURL: artURL))
-////                                }
-////                                if self.albums.count > 0 {
-////                                    print(self.albums[self.albums.count-1].artistName)
-////                                }
-////                            }
-////                        }
-//                    }
-//                }
-//            }
-//        }
-//        print("Albums post request: \n")
-//        print(albums)
-//    }
-
